d85d28382fec6dbec2845c98cce7d64482664f8f
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-09-07 17:52:42 +0200

    Update version of pasteur

--- config.ml
+++ config.ml
@@ -6,9 +6,37 @@ let remote =
   let doc = Key.Arg.info ~doc:"Remote Git repository." [ "r"; "remote" ] in
   Key.(create "remote" Arg.(opt string "git://127.0.0.1/pasteur" doc))
 
-let port =
+let http_port =
   let doc = Key.Arg.info ~doc:"port of HTTP service" [ "p"; "port" ] in
-  Key.(create "port" Arg.(opt int 4343 doc))
+  Key.(create "http-port" Arg.(opt int 80 doc))
+
+let https_port =
+  let doc = Key.Arg.info ~doc:"port of HTTPS service" [ "https-port" ] in
+  Key.(create "https-port" Arg.(opt int 443 doc))
+
+let dns_key =
+  let doc = Key.Arg.info ~doc:"DNS key" [ "dns-key" ] in
+  Key.(create "dns_key" Arg.(required string doc))
+
+let dns_port =
+  let doc = Key.Arg.info ~doc:"DNS server port" [ "dns-port" ] in
+  Key.(create "dns-port" Arg.(opt int 53 doc))
+
+let dns_server =
+  let doc = Key.Arg.info ~doc:"DNS server" [ "dns-server" ] in
+  Key.(create "dns-server" Arg.(required ipv4_address doc))
+
+let hostname =
+  let doc = Key.Arg.info ~doc:"Hostname of the unikernel" [ "hostname" ] in
+  Key.(create "hostname" Arg.(required string doc))
+
+let ssh_seed =
+  let doc = Key.Arg.info ~doc:"Seed of the private SSH key" [ "ssh-seed" ] in
+  Key.(create "ssh-seed" Arg.(opt (some string) None doc))
+
+let ssh_auth =
+  let doc = Key.Arg.info ~doc:"SSH Public key of the remote Git endpoint" [ "ssh-auth" ] in
+  Key.(create "ssh-auth" Arg.(opt (some string) None doc))
 
 let random_len =
   let doc = Key.Arg.info ~doc:"Length of generated URI" [ "length" ] in
@@ -16,8 +44,17 @@ let random_len =
 
 let pasteur =
   foreign "Unikernel.Make"
-    ~keys:[ Key.abstract remote; Key.abstract port; Key.abstract random_len ]
-    (random @-> console @-> time @-> pclock @-> kv_ro @-> stackv4 @-> resolver @-> conduit @-> job)
+    ~keys:[ Key.abstract remote
+          ; Key.abstract http_port
+          ; Key.abstract https_port
+          ; Key.abstract dns_key
+          ; Key.abstract dns_server
+          ; Key.abstract dns_port
+          ; Key.abstract ssh_seed
+          ; Key.abstract ssh_auth
+          ; Key.abstract hostname
+          ; Key.abstract random_len ]
+    (random @-> console @-> time @-> mclock @-> pclock @-> kv_ro @-> stackv4 @-> job)
 
 let stack = generic_stackv4 default_network
 let conduit = conduit_direct stack
@@ -26,22 +63,19 @@ let console = console
 let public = generic_kv_ro "public"
 
 let packages =
-  let multipart_form = "git+https://github.com/dinosaure/multipart_form.git" in
-  let tuyau = "git+https://github.com/dinosaure/tuyau.git" in
   let paf = "git+https://github.com/dinosaure/paf-le-chien.git" in
 
   [ package "httpaf"
   ; package "uuidm"
   ; package "tyxml"
   ; package "irmin-mirage-git"
-
-  ; package ~pin:tuyau "tuyau"
-  ; package ~pin:tuyau "tuyau-tls"
-  ; package ~pin:tuyau ~sublibs:["tcp"] "tuyau-mirage"
-  ; package ~pin:multipart_form "multipart_form"
+  ; package ~sublibs:[ "dns" ] "conduit-mirage"
+  ; package ~sublibs:[ "mirage" ] "dns-certify"
+  ; package "awa-conduit"
+  ; package "multipart_form"
   ; package ~pin:paf "paf" ]
 
 let () =
   register "pasteur"
     ~packages
-    [ pasteur $ default_random $ default_console $ default_time $ default_posix_clock $ public $ stack $ resolver $ conduit ]
+    [ pasteur $ default_random $ default_console $ default_time $ default_monotonic_clock $ default_posix_clock $ public $ stack ]
