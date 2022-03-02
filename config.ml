81b1c8ee4f36195ce931cdc22a8017b977753042
clecat <charles-edouard.lecat@epitech.eu>
2022-03-02 16:26:11 +0100

    Update to mirage 4.0

--- config.ml
+++ config.ml
@@ -2,130 +2,13 @@
 
 open Mirage
 
-type mimic = Mimic
-
-let mimic = typ Mimic
-
-let mimic_count =
-  let v = ref (-1) in
-  fun () -> incr v ; !v
-
-let mimic_conf () =
-  let packages = [ package "mimic" ] in
-  impl @@ object
-       inherit base_configurable
-       method ty = mimic @-> mimic @-> mimic
-       method module_name = "Mimic.Merge"
-       method! packages = Key.pure packages
-       method name = Fmt.str "merge_ctx%02d" (mimic_count ())
-       method! connect _ _modname =
-         function
-         | [ a; b ] -> Fmt.str "Lwt.return (Mimic.merge %s %s)" a b
-         | [ x ] -> Fmt.str "%s.ctx" x
-         | _ -> Fmt.str "Lwt.return Mimic.empty"
-     end
-
-let merge ctx0 ctx1 = mimic_conf () $ ctx0 $ ctx1
-
-let mimic_tcp_conf =
-  let packages = [ package "git-mirage" ~sublibs:[ "tcp" ] ] in
-  impl @@ object
-       inherit base_configurable
-       method ty = stackv4 @-> mimic
-       method module_name = "Git_mirage_tcp.Make"
-       method! packages = Key.pure packages
-       method name = "tcp_ctx"
-       method! connect _ modname = function
-         | [ stack ] ->
-           Fmt.str {ocaml|Lwt.return (%s.with_stack %s %s.ctx)|ocaml}
-             modname stack modname
-         | _ -> assert false
-     end
-
-let mimic_tcp_impl stackv4 = mimic_tcp_conf $ stackv4
-
-let mimic_ssh_conf ~kind ~seed ~auth =
-  let seed = Key.abstract seed in
-  let auth = Key.abstract auth in
-  let packages = [ package "git-mirage" ~sublibs:[ "ssh" ] ] in
-  impl @@ object
-       inherit base_configurable
-       method ty = stackv4 @-> mimic @-> mclock @-> mimic
-       method! keys = [ seed; auth; ]
-       method module_name = "Git_mirage_ssh.Make"
-       method! packages = Key.pure packages
-       method name = match kind with
-         | `Rsa -> "ssh_rsa_ctx"
-         | `Ed25519 -> "ssh_ed25519_ctx"
-       method! connect _ modname =
-         function
-         | [ _; tcp_ctx; _ ] ->
-             let with_key =
-               match kind with
-               | `Rsa -> "with_rsa_key"
-               | `Ed25519 -> "with_ed25519_key"
-             in
-             Fmt.str
-               {ocaml|let ssh_ctx00 = Mimic.merge %s %s.ctx in
-                      let ssh_ctx01 = Option.fold ~none:ssh_ctx00 ~some:(fun v -> %s.%s v ssh_ctx00) %a in
-                      let ssh_ctx02 = Option.fold ~none:ssh_ctx01 ~some:(fun v -> %s.with_authenticator v ssh_ctx01) %a in
-                      Lwt.return ssh_ctx02|ocaml}
-               tcp_ctx modname
-               modname with_key Key.serialize_call seed
-               modname Key.serialize_call auth
-         | _ -> assert false
-     end
-
-let mimic_ssh_impl ~kind ~seed ~auth stackv4 mimic_git mclock =
-  mimic_ssh_conf ~kind ~seed ~auth
-  $ stackv4
-  $ mimic_git
-  $ mclock
-
-(* TODO(dinosaure): user-defined nameserver and port. *)
-
-let mimic_dns_conf =
-  let packages = [ package "git-mirage" ~sublibs:[ "dns" ] ] in
-  impl @@ object
-       inherit base_configurable
-       method ty = random @-> mclock @-> time @-> stackv4 @-> mimic @-> mimic
-       method module_name = "Git_mirage_dns.Make"
-       method! packages = Key.pure packages
-       method name = "dns_ctx"
-       method! connect _ modname =
-         function
-         | [ _; _; _; stack; tcp_ctx ] ->
-             Fmt.str
-               {ocaml|let dns_ctx00 = Mimic.merge %s %s.ctx in
-                      let dns_ctx01 = %s.with_dns %s dns_ctx00 in
-                      Lwt.return dns_ctx01|ocaml}
-               tcp_ctx modname
-               modname stack
-         | _ -> assert false
-     end
-
-let mimic_dns_impl random mclock time stackv4 mimic_tcp =
-  mimic_dns_conf $ random $ mclock $ time $ stackv4 $ mimic_tcp
-
 let remote =
   let doc = Key.Arg.info ~doc:"Remote Git repository." [ "r"; "remote" ] in
-  Key.(create "remote" Arg.(opt string "git://127.0.0.1/pasteur" doc))
+  Key.(create "remote" Arg.(required string doc))
 
 let port =
   let doc = Key.Arg.info ~doc:"port of HTTP service." [ "p"; "port" ] in
   Key.(create "port" Arg.(opt (some int) None doc))
-
-let dns_key =
-  let doc = Key.Arg.info ~doc:"DNS key." [ "dns-key" ] in
-  Key.(create "dns_key" Arg.(opt (some string) None doc))
-
-let dns_port =
-  let doc = Key.Arg.info ~doc:"DNS server port." [ "dns-port" ] in
-  Key.(create "dns_port" Arg.(opt int 53 doc))
-
-let dns_addr =
-  let doc = Key.Arg.info ~doc:"DNS server." [ "dns-server" ] in
-  Key.(create "dns_addr" Arg.(opt (some ipv4_address) None doc))
 
 let email =
   let doc = Key.Arg.info ~doc:"Let's encrypt email." [ "email" ] in
@@ -133,27 +16,35 @@ let email =
 
 let hostname =
   let doc = Key.Arg.info ~doc:"Hostname of the unikernel." [ "hostname" ] in
-  Key.(create "hostname" Arg.(required string doc))
-
-let ssh_seed =
-  let doc = Key.Arg.info ~doc:"Seed of the private SSH key." [ "ssh-seed" ] in
-  Key.(create "ssh_seed" Arg.(opt (some string) None doc))
-
-let ssh_auth =
-  let doc = Key.Arg.info ~doc:"SSH Public key of the remote Git endpoint." [ "ssh-auth" ] in
-  Key.(create "ssh_auth" Arg.(opt (some string) None doc))
+  Key.(create "hostname" Arg.(opt (some string) None doc))
 
 let random_len =
   let doc = Key.Arg.info ~doc:"Length of generated URI." [ "length" ] in
-  Key.(create "random_length" Arg.(opt int 3 doc))
+  Key.(create "random-length" Arg.(opt int 3 doc))
 
 let cert_seed =
   let doc = Key.Arg.info ~doc:"Let's encrypt certificate seed." [ "cert-seed" ] in
-  Key.(create "cert_seed" Arg.(opt (some string) None doc))
+  Key.(create "cert-seed" Arg.(opt (some string) None doc))
+
+let cert_key_type =
+  let doc = Key.Arg.info ~doc:"certificate key type" [ "cert-key-type" ] in
+  Key.(create "cert-key-type" Arg.(opt string "RSA" doc))
+
+let cert_bits =
+  let doc = Key.Arg.info ~doc:"certificate public key bits" [ "cert-bits" ] in
+  Key.(create "cert-bits" Arg.(opt int 4096 doc))
 
 let account_seed =
   let doc = Key.Arg.info ~doc:"Let's encrypt account seed." [ "account-seed" ] in
-  Key.(create "account_seed" Arg.(opt (some string) None doc))
+  Key.(create "account-seed" Arg.(opt (some string) None doc))
+
+let account_key_type =
+  let doc = Key.Arg.info ~doc:"account key type" [ "account-key-type" ] in
+  Key.(create "account-key-type" Arg.(opt string "RSA" doc))
+
+let account_bits =
+  let doc = Key.Arg.info ~doc:"account public key bits" [ "account-bits" ] in
+  Key.(create "account-bits" Arg.(opt int 4096 doc))
 
 let production =
   let doc = Key.Arg.info ~doc:"Let's encrypt production environment." [ "production" ] in
@@ -163,60 +54,88 @@ let https =
   let doc = Key.Arg.info ~doc:"Start an HTTP server with a TLS certificate." [ "https" ] in
   Key.(create "https" Arg.(opt bool false doc))
 
+let ssh_key =
+  let doc = Key.Arg.info ~doc:"Private ssh key (rsa:<seed> or ed25519:<b64-key>)." ["ssh-key"] in
+  Key.(create "ssh-key" Arg.(opt (some string) None doc))
+
+let ssh_authenticator =
+  let doc = Key.Arg.info ~doc:"SSH host key authenticator." [ "ssh-authenticator" ] in
+  Key.(create "ssh_authenticator" Arg.(opt (some string) None doc))
+
+let tls_authenticator =
+  let doc = Key.Arg.info ~doc:"TLS host authenticator." [ "tls-authenticator" ] in
+  Key.(create "https_authenticator" Arg.(opt (some string) None doc))
+
+let pasteur_js =
+  let dune _info =
+    [ Dune.stanzaf {dune|(subdir public
+ (rule
+  (enabled_if (= %%{context_name} "default"))
+  (deps ../js/pasteur_js.bc.js)
+  (target pasteur.js)
+  (action (copy %%{deps} %%{target}))))|dune} ] in
+  impl ~dune "Pasteur_js" job
+
+let pasteur_hljs =
+  let dune _info =
+    [ Dune.stanzaf {dune|(rule
+ (enabled_if (= %%{context_name} "default"))
+ (mode promote)
+ (deps js/pasteur_hljs.bc.js public/highlight.js)
+ (target language.default.ml)
+ (action (with-stdout-to
+  %%{target} (bash "node js/pasteur_hljs.bc.js"))))|dune}
+    ; Dune.stanzaf {dune|(rule
+ (target language.ml)
+ (deps %%{exe:language.default.ml})
+ (action (copy %%{exe:language.default.ml} %%{target})))|dune} ] in
+  let files _ = [ Fpath.v "language.ml" ] in
+  impl ~files ~dune "Pasteur_hljs" job
+
 let pasteur =
   foreign "Unikernel.Make"
-    ~keys:[ Key.abstract remote
-          ; Key.abstract port
-          ; Key.abstract https
-          ; Key.abstract dns_key
-          ; Key.abstract dns_addr
-          ; Key.abstract dns_port
-          ; Key.abstract ssh_seed
-          ; Key.abstract ssh_auth
-          ; Key.abstract email
-          ; Key.abstract hostname
-          ; Key.abstract random_len
-          ; Key.abstract cert_seed
-          ; Key.abstract account_seed
-          ; Key.abstract production ]
-    (random @-> console @-> time @-> mclock @-> pclock @-> kv_ro
-     @-> mimic
-     @-> stackv4 @-> stackv4v6 @-> job)
+    ~deps:[ dep pasteur_js; dep pasteur_hljs ]
+    ~packages:[ package "brr" ~build:true ~scope:`Switch ]
+    ~keys:[ Key.v remote
+          ; Key.v port
+          ; Key.v https
+          ; Key.v email
+          ; Key.v hostname
+          ; Key.v random_len
+          ; Key.v cert_seed
+          ; Key.v cert_key_type
+          ; Key.v cert_bits
+          ; Key.v account_seed
+          ; Key.v account_key_type
+          ; Key.v account_bits
+          ; Key.v production ]
+    (random @-> console @-> time @-> mclock @-> pclock @-> kv_ro @-> stackv4v6 @-> dns_client @-> git_client @-> job)
 
-let mimic ~kind ~seed ~auth stackv4 random mclock time =
-  let mtcp = mimic_tcp_impl stackv4 in
-  let mdns = mimic_dns_impl random mclock time stackv4 mtcp in
-  let mssh = mimic_ssh_impl ~kind ~seed ~auth stackv4 mtcp mclock in
-  merge mssh mdns
-
-let random = default_random
-let mclock = default_monotonic_clock
-let time = default_time
-let stackv4 = generic_stackv4 default_network
 let stack = generic_stackv4v6 default_network
-let conduit = conduit_direct ~tls:true stackv4
-let resolver = resolver_dns stackv4
-let console = console
-let public = generic_kv_ro "public"
+let dns = generic_dns_client stack
 
-let mimic = mimic ~kind:`Rsa ~seed:ssh_seed ~auth:ssh_auth stackv4 random mclock time
+let git =
+  let git = git_happy_eyeballs stack dns (generic_happy_eyeballs stack dns) in
+  let tcp = tcpv4v6_of_stackv4v6 stack in
+  merge_git_clients (git_tcp tcp git)
+    (merge_git_clients (git_ssh ~key:ssh_key ~authenticator:ssh_authenticator tcp git)
+                       (git_http ~authenticator:tls_authenticator tcp git))
+
+let public = docteur ~extra_deps:[ "public/pasteur.js" ] "relativize://public/"
 
 let packages =
-  [ package "httpaf"
+  [ package "paf" ~min:"0.0.9"
+  ; package "paf" ~sublibs:[ "mirage" ] ~min:"0.0.9"
+  ; package "paf-le" ~min:"0.0.9"
   ; package "uuidm"
   ; package "tyxml"
-  ; package "git-mirage" ~min:"3.3.1"
-  ; package "irmin-mirage-git" ~min:"2.3.0"
-  ; package ~sublibs:[ "mirage" ] "dns-certify"
-  ; package "multipart_form"
-  ; package "paf"
+  ; package "irmin-mirage-git"
+  ; package "multipart_form-lwt"
   ; package "ocplib-json-typed"
-  ; package "ezjsonm"
-  ; package ~sublibs:[ "le" ] "paf" ]
+  ; package "ezjsonm" ]
 
-let () =
+  let () =
   register "pasteur"
     ~packages
-    [ pasteur $ random $ default_console $ time $ mclock $ default_posix_clock $ public
-      $ mimic
-      $ stackv4 $ stack ]
+    [ pasteur $ default_random $ default_console $ default_time $ default_monotonic_clock $ default_posix_clock $ public
+      $ stack $ dns $ git ]
